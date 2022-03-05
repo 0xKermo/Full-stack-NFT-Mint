69081878c03d76147068091cbf6f5809f202e0fb
behzatce <behzatc3@hotmail.com>
2022-03-05 15:09:55 +0300

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
 
   const NFT = await hre.ethers.getContractFactory("Test");
-  const nft = await NFT.deploy("Test","TST","0xd4453790033a2bd762f526409b7f358023773723d9e9bc42487e4996869162b6");
+  const nft = await NFT.deploy("Test","TST","merkle root");
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
 }
